b20107ab786865a752529dbb6643abffd5d3ffc6
jaime-conde <jaime.conde.acos@gmail.com>
2026-09-17 11:56:17 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import "./project-heading-fix.css";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 const assetPrefix = process.env.GITHUB_ACTIONS === "true" ? "/portfolio" : "";
-const favicon = `${assetPrefix}/favicon-jc.png?v=20260917-2`;
+const favicon = `${assetPrefix}/favicon-jc.png?v=3`;
 
 export const metadata: Metadata = {
   title: "Portfolio",
